60ce5a23280e8014d812ffa7a06dcec8ef727eae
Maryam Lotfi <lotfimaryam1110@gmail.com>
2025-08-07 02:16:23 -0900

    change save

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import FCMDeviceCreateUpdateView, FCMDeviceListView, FCMDeviceDeleteView, TestNotificationView
+from .views import FCMDeviceCreateUpdateView, FCMDeviceListView, FCMDeviceDeleteView
 from . import views # یا از app.views import test_firebase_firestore
 
 urlpatterns = [
     path('fcm-device/', FCMDeviceCreateUpdateView.as_view(), name='fcm_device_register_update'),
     path('device/list/', FCMDeviceListView.as_view(), name='fcm_device_list'),
     path('device/delete/<int:pk>/', FCMDeviceDeleteView.as_view(), name='fcm_device_delete'),
-    path('test-notification/', TestNotificationView.as_view(), name='test-notification'),
 ]
